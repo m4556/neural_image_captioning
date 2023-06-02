346a099f66eb242f8c1d4136278a187be0216b53
m4556 <101812065+m4556@users.noreply.github.com>
2023-06-02 03:20:25 +0200

    add files

--- utils.py
+++ utils.py
@@ -21,37 +21,22 @@ def print_examples(model, device, dataset):
         "Example 1 OUTPUT: "
         + " ".join(model.caption_image(test_img1.to(device), dataset.vocab))
     )
-    test_img2 = transform(
-        Image.open("test/child.jpg").convert("RGB")
-    ).unsqueeze(0)
-    print("Example 2 CORRECT: Child holding red frisbee outdoors")
-    print(
-        "Example 2 OUTPUT: "
-        + " ".join(model.caption_image(test_img2.to(device), dataset.vocab))
-    )
+    
     test_img3 = transform(Image.open("test/bus.png").convert("RGB")).unsqueeze(
         0
     )
-    print("Example 3 CORRECT: Bus driving by parked cars")
+    print("Example 2 CORRECT: Bus driving by parked cars")
     print(
-        "Example 3 OUTPUT: "
+        "Example 2 OUTPUT: "
         + " ".join(model.caption_image(test_img3.to(device), dataset.vocab))
     )
     test_img4 = transform(
         Image.open("test/boat.png").convert("RGB")
     ).unsqueeze(0)
-    print("Example 4 CORRECT: A small boat in the ocean")
+    print("Example 3 CORRECT: A small boat in the ocean")
     print(
-        "Example 4 OUTPUT: "
+        "Example 3 OUTPUT: "
         + " ".join(model.caption_image(test_img4.to(device), dataset.vocab))
-    )
-    test_img5 = transform(
-        Image.open("test/horse.png").convert("RGB")
-    ).unsqueeze(0)
-    print("Example 5 CORRECT: A cowboy riding a horse in the desert")
-    print(
-        "Example 5 OUTPUT: "
-        + " ".join(model.caption_image(test_img5.to(device), dataset.vocab))
     )
     model.train()
 
